a6489da3d5f60550d0d259717ea6967b08bd64fa
Gregorius Soedharmo <arkatufus@yahoo.com>
2023-09-23 03:55:48 +0700

    Fix unit tests

--- src/Akka.Logger.NLog.Tests/NLogFormattingSpecs.cs
+++ src/Akka.Logger.NLog.Tests/NLogFormattingSpecs.cs
@@ -1,16 +1,19 @@
+using System;
+using System.Diagnostics;
 using System.Linq;
+using System.Text.RegularExpressions;
 using System.Threading;
 using System.Threading.Tasks;
 using Akka.Actor;
 using Akka.Configuration;
 using Akka.Event;
+using FluentAssertions;
 using FluentAssertions.Extensions;
 using NLog;
 using NLog.Targets;
 using Xunit;
 using Xunit.Abstractions;
 using LogLevel = Akka.Event.LogLevel;
-using FluentAssertions;
 
 namespace Akka.Logger.NLog.Tests
 {
@@ -19,6 +22,7 @@ namespace Akka.Logger.NLog.Tests
         private static readonly Config Config = "akka.loglevel = DEBUG";
 
         private readonly ILoggingAdapter _loggingAdapter;
+        private readonly object _lock = new ();
         const string LogSourceName = "my-test-system";
 
         public NLogFormattingSpecs(ITestOutputHelper helper) : base(Config, output: helper)
@@ -40,65 +44,63 @@ namespace Akka.Logger.NLog.Tests
         [InlineData(LogLevel.InfoLevel, "test case {a}", new object[] { 1 }, "Info|test case 1")]
         [InlineData(LogLevel.WarningLevel, "test case {b}", new object[] { "2" }, "Warn|test case \"2\"")]
         [InlineData(LogLevel.ErrorLevel, "test case {c}", new object[] { 3.0 }, "Error|test case 3")]
-        public async Task LoggingTest(LogLevel level, string formatStr, object[] formatArgs, string resultStr)
+        public void LoggingTest(LogLevel level, string formatStr, object[] formatArgs, string resultStr)
         {
             var loggingTarget = new MemoryTarget { Layout = "${level}|${message}" };
             LogManager.Setup().LoadConfiguration(c => c.ForLogger().WriteTo(loggingTarget));
 
-            await WaitForRadioSilence(loggingTarget);
             _loggingAdapter.Log(level, formatStr, formatArgs);
 
-            await AwaitConditionAsync(() => loggingTarget.Logs.Count != 0, 3.Seconds(), 10.Milliseconds());
-
-            loggingTarget.Logs.Should().NotBeEmpty();
-            loggingTarget.Logs.First().Should().Be(resultStr);
+            AssertLogMessage(loggingTarget, resultStr, 5.Seconds()).Should().BeTrue();
         }
 
         [Theory]
         [InlineData(LogLevel.InfoLevel, "test case {0}", new object[] { 1 }, "{0}|{1}|test case 1")]
-        public async Task LoggingTestWithEventProperties(LogLevel level, string formatStr, object[] formatArgs, string resultStr)
+        public void LoggingTestWithEventProperties(LogLevel level, string formatStr, object[] formatArgs, string resultStr)
         {
-            _loggingAdapter.Log(level, formatStr, formatArgs);
             var loggingTarget = new MemoryTarget
                 {Layout = "${event-properties:item=logSource}|${event-properties:item=threadId:format=D4}|${message}" };
             LogManager.Setup().LoadConfiguration(c => c.ForLogger().WriteTo(loggingTarget));
 
-            await WaitForRadioSilence(loggingTarget);
             _loggingAdapter.Log(level, formatStr, formatArgs);
-
-            await AwaitConditionAsync(() => loggingTarget.Logs.Count != 0, 3.Seconds(), 10.Milliseconds());
-
-            var formattedResultString = string.Format(resultStr, LogSourceName, "*");
-
-            loggingTarget.Logs.Should().NotBeEmpty();
-            loggingTarget.Logs.First().Should().Match(formattedResultString);
+            
+            var formattedResultString = string.Format(resultStr, LogSourceName, 
+                Thread.CurrentThread.ManagedThreadId.ToString().PadLeft(4, '0'));
+            
+            AssertLogMessage(loggingTarget, formattedResultString, 5.Seconds()).Should().BeTrue();
         }
 
         [Theory]
         [InlineData(LogLevel.InfoLevel, "test {color} case", new object[] { "Red" }, "test {{color}} case|color=Red, logSource={0}, actorPath={1}, threadId={2}")]
-        public async Task LoggingWithStructuredLogging(LogLevel level, string formatStr, object[] formatArgs, string resultStr)
+        public void LoggingWithStructuredLogging(LogLevel level, string formatStr, object[] formatArgs, string resultStr)
         {
-            var loggingTarget = new global::NLog.Targets.MemoryTarget { Layout = "${message:raw=true}|${all-event-properties}" };
+            var loggingTarget = new MemoryTarget { Layout = "${message:raw=true}|${all-event-properties}" };
             LogManager.Setup().LoadConfiguration(c => c.ForLogger().WriteTo(loggingTarget));
 
-            await WaitForRadioSilence(loggingTarget);
             _loggingAdapter.Log(level, formatStr, formatArgs);
 
-            await AwaitConditionAsync(() => loggingTarget.Logs.Count != 0, 3.Seconds(), 10.Milliseconds());
-
-            var formattedResultString = string.Format(resultStr, LogSourceName, TestActor.Path, "*");
-
-            loggingTarget.Logs.Should().NotBeEmpty();
-            loggingTarget.Logs.First().Should().Match(formattedResultString);
+            var formattedResultString = string.Format(resultStr, LogSourceName, TestActor.Path, Thread.CurrentThread.ManagedThreadId.ToString());
+            AssertLogMessage(loggingTarget, formattedResultString, 5.Seconds()).Should().BeTrue();
         }
 
-        private static async Task WaitForRadioSilence(MemoryTarget target)
+        private bool AssertLogMessage(MemoryTarget target, string expected, TimeSpan timeout)
         {
-            while (target.Logs.Count > 0)
+            var stopwatch = Stopwatch.StartNew();
+            while (stopwatch.Elapsed < timeout)
             {
-                target.Logs.Clear();
-                await Task.Delay(1000);
+                Thread.Sleep(10);
+                
+                string[] array;
+                lock (_lock)
+                {
+                    array = target.Logs.ToArray();
+                    target.Logs.Clear();
+                }
+
+                if (array.Any(s => s.Equals(expected)))
+                    return true;
             }
+            return false;
         }
     }
 }
